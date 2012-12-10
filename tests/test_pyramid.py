14aa460d451f467d1ae789118456a1e9e0c9dbbf
jhnwsk <jhnwsk@gmail.com>
2012-12-10 23:54:58 +0100

    remove pdb traces

--- tests/test_pyramid.py
+++ tests/test_pyramid.py
@@ -16,9 +16,6 @@ class TemplateUsageReportTestMixin(PluginTester):
     def makeSuite(self):
         class TestCase(unittest.TestCase):
             def runTest(_self):
-
-                import nose.tools
-                nose.tools.set_trace()
                 render(self.TEMPLATE_NAME, {})
         return unittest.TestSuite([TestCase()])
 
